6e3fba97a8e25b723942d4dbdc8df7dc1e835da9
Jannick Mørup <jannickmorup@gmail.com>
2023-05-01 12:43:11 +0200

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACM.BL
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if(!string.IsNullOrEmpty(fullName) )
+                {
+                    if(!string.IsNullOrWhiteSpace(FullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        private string _LastName
+        {
+            get 
+            { 
+                return _LastName; 
+            }
+            set
+            {
+                _LastName = value; 
+            }
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+            if(string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if(string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+            return isValid;
+        }
+    }
+}
